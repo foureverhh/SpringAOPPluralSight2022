a1d51e54ed1c04f8e2bb63f6a131822f1c74ae4b
liyuan <yuanheng.li@skolverket.se>
2022-09-14 16:15:03 +0200

    init

---
+++ src/main/java/com/example/pluralsightspringaop2022/PluralSightSpringAop2022Application.java
@@ -0,0 +1,13 @@
+package com.example.pluralsightspringaop2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PluralSightSpringAop2022Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PluralSightSpringAop2022Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pluralsightspringaop2022/PluralSightSpringAop2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pluralsightspringaop2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PluralSightSpringAop2022ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
